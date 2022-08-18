3dd19649ebb0ba6d515fd140399afc589cfee368
Usuario <Usuario@CPD03-M07>
2022-08-18 14:55:54 -0300

    Adicionar arquivos de projeto.

---
+++ AppTela_login/AppTela_login/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTela_login
+{
+    public partial class App : Application
+    {
+        public List<DadosUsuario> list_usuario =new List<DadosUsuario>
+        {
+           new dadosUsuario()
+           {
+               Email = "aluno@etec" ,
+               Nome = "Aluno" ,
+               Senha = "123"
+
+           },
+           new dadosUsuario()
+           {
+             Email ="prof@etec" ,
+             Nome = "Professor" ,
+             Senha = "456"
+           }
+        }
+    }
+}
